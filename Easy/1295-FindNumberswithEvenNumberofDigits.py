6238e77c6f4d325c3afd5e833b46369cbb4c4771
Alay <62489788+alaykumar@users.noreply.github.com>
2023-09-27 12:17:00 -0400

    Create 1295-FindNumberswithEvenNumberofDigits.py

---
+++ Easy/1295-FindNumberswithEvenNumberofDigits.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        # counter variable for even numbers
+        count = 0
+        
+        # for loop to iterate through the array
+        for num in nums:
+            
+            # if the array value length mod 2 is 0 then increae the count
+            if len(str(num)) % 2 == 0:
+                count += 1
+        # return the count
+        return count
