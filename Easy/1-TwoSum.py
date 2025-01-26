14376b4c5f3ac459d2ad5f2e0e751420b2aba37d
Alay <62489788+alaykumar@users.noreply.github.com>
2025-01-26 07:12:23 -0500

    Update 1-TwoSum.py

--- Easy/1-TwoSum.py
+++ Easy/1-TwoSum.py
@@ -3,21 +3,17 @@ class Solution:
 
         # Create a hash table (dictionary) to store indicies of array numbers
         ans = {}
-
         
         l = len(nums)
 
-        # for loop to iterate through the array
         for i in range(l):
 
             # calculate the difference between the target and array value
             diff = target - nums[i]
 
-            # if the difference is in the hash map then return the hash map value and the current 
-            # numbers indicies otherwise add the indicices and the value in the hashmap
+            # if difference exists in dict, return the answer
             if diff in ans:
                 return [ans[diff], i]
             ans[nums[i]] = i
 
-        # return ampty array if no pair is found
         return []
