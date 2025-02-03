30163b7106b0cf1087fcf5b1da7271748b3ce704
Alay <62489788+alaykumar@users.noreply.github.com>
2025-02-02 22:11:20 -0500

    Update 1598.py

--- Easy/1598.py
+++ Easy/1598.py
@@ -1,12 +1,11 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
         
-        # create stack to append any logs with folder names
         stack = []
 
         # for loop to iterate through the logs
         for log in logs:
-            # if the log begin swith '../' then pop from the stack of folder nams
+            # if the log begin swith '../' then pop from the stack of folder names
             if log.startswith('../'):
                 if stack:
                     stack.pop()
@@ -16,11 +15,9 @@ class Solution:
             elif log.startswith('./'):
                 continue
             
-            # append the log enttry in the stack if its not a command to go back a directory 
-            # or to remian in the same directory
             else:
                 stack.append(log)
 
-        # return the remaining length of the stack
+        
         return len(stack)
         
