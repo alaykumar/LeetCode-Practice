09e670999ab02ca9bbc99a8f24a560a41794cd0c
Alay <62489788+alaykumar@users.noreply.github.com>
2025-02-12 22:01:29 -0500

    Update 485-MaxConsecutiveOnes.py

--- Easy/485-MaxConsecutiveOnes.py
+++ Easy/485-MaxConsecutiveOnes.py
@@ -5,11 +5,8 @@ class Solution(object):
         :rtype: int
         """
 
-        # initialize variables for counter and max count
         count = 0
         mx = 0
-
-        # length of the nums array
         l = len(nums)
 
        # for loop to iterate through the array 
@@ -18,12 +15,11 @@ class Solution(object):
            # if the arr value is 1 then increase the count
             if nums[num] == 1:
                 count+=1
-            # else reset count back to 0
             else:
                 count = 0
             # store the maximum of eother the current max or the old max into the mx varibale
             mx = max(mx, count)
 
-       # return the max
+       
         return mx
         
